ad943f777f8d43dac5d62bae2d42689a945c0ba3
Artem Nishchenko <artem.nishchenko0@gmail.com>
2020-10-12 16:00:40 +0300

    Initial commit

---
+++ app/src/main/java/ua/pkk/wetravel/LoginActivity.java
@@ -0,0 +1,22 @@
+package ua.pkk.wetravel;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void onLoginBtnClick(View view) {
+
+    }
+
+
+}
