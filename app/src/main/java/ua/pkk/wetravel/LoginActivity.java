dd5ed37c6a8b880f5edd4f29631e31198516c2e7
Artem Nishchenko <artem.nishchenko0@gmail.com>
2020-10-14 13:44:48 +0300

    Add json

--- app/src/main/java/ua/pkk/wetravel/LoginActivity.java
+++ app/src/main/java/ua/pkk/wetravel/LoginActivity.java
@@ -1,22 +1,69 @@
 package ua.pkk.wetravel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextInputEditText mail;
+    private TextInputEditText password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        init(mAuth.getCurrentUser());
+        mail = findViewById(R.id.mail_ed);
+        password = findViewById(R.id.passw_ed);
+    }
+
+    private void init(FirebaseUser currentUser) {
+        if (currentUser == null) return;
+        startActivity(new Intent(getBaseContext(),MainActivity.class));
     }
 
 
     public void onLoginBtnClick(View view) {
+        if (!isValidEmail(mail.getText().toString())){
+            mail.setError(getApplicationContext().getResources().getString(R.string.isnt_valid_mail));
+            return;
+        }
+        if (password.getText().toString().isEmpty()) {
+            password.setError(getApplicationContext().getResources().getString(R.string.empty_password));
+        }
+        mAuth.signInWithEmailAndPassword(mail.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(getBaseContext(),MainActivity.class));
+                        } else {
+                            //TODO Show errorq
 
+                        }
+                    }
+                });
     }
 
 
+    public void onRegisterBtnClick(View view) {
+        //TODO go to register activity
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return target != null && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 }
--- app/src/main/java/ua/pkk/wetravel/MainActivity.java
+++ app/src/main/java/ua/pkk/wetravel/MainActivity.java
@@ -2,7 +2,6 @@ package ua.pkk.wetravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-        finish();
     }
 }
